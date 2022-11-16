1a517e26ffce02e7101f7e82037d0090c10b89e7
Veit Hueter <info@pinkblau.de>
2022-11-16 04:44:42 +0100

    deployment fixes

--- db.js
+++ db.js
@@ -3,9 +3,10 @@
 const spicedPg = require("spiced-pg");
 require("dotenv").config();
 
-const { DB_USER, DB_PASS, DB_DATABASE } = process.env;
+const { DB_USER, DB_PASS, DB_DATABASE, DATABASE_URL } = process.env;
 const db = spicedPg(
-    `postgres:${DB_USER}:${DB_PASS}@localhost:5432/${DB_DATABASE}`
+    // `postgres:${DB_USER}:${DB_PASS}@localhost:5432/${DB_DATABASE}`
+    `${DATABASE_URL}`
 );
 
 //  ++++++++++++++ END setup modules
